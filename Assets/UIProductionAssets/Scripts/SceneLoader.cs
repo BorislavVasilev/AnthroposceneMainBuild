913edb342d6735d9bea1d4378c5adcf381499111
BorislavVasilev <BVASIL200@caledonian.ac.uk>
2017-03-05 19:04:51 +0000

    NewSceneLoading
    TestSceneForLoading + Script

---
+++ Assets/UIProductionAssets/Scripts/SceneLoader.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+    public float m_minDuration = 1;
+
+    public IEnumerator LoadScene(string sceneName)
+    {
+                
+        // Load loading screen
+        yield return SceneManager.LoadSceneAsync("LoadingScene");
+
+        // !!! unload old screen (automatic)
+
+       
+        float endTime = Time.time + m_minDuration;
+
+        // Load level async
+        yield return SceneManager.LoadSceneAsync(sceneName);
+
+        if (Time.time < endTime)
+
+            yield return new WaitForSeconds(endTime - Time.time);
+
+        // !!! unload loading screen
+        SceneManager.UnloadSceneAsync("LoadingScene");
+
+        
+    }
+
+    public void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
