399ac9a0ad78f1954c13afa530a4a731ea1b5e73
BorislavVasilev <BVASIL200@caledonian.ac.uk>
2017-03-15 15:23:55 +0000

    GeneralTest and The Pause Button Function

--- Assets/UIProductionAssets/Scripts/PauseScript.cs
+++ Assets/UIProductionAssets/Scripts/PauseScript.cs
@@ -24,6 +24,7 @@ public class PauseScript : MonoBehaviour {
         if (!pause)
         {
             Time.timeScale = 1;
+            
             pauseMenu.SetActive(false);
             pauseButton.SetActive(true);
             settingsButton.SetActive(true);
